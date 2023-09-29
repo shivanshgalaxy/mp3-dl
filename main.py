4b1a91835e0778e2ee99990f26d25e783bc15589
shivanshgalaxy <shivansh.galaxy@gmail.com>
2023-09-29 23:43:09 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from sys import exit, stderr
+from pytube import YouTube
+from pytube.exceptions import *
+
+# TODO - Add functionality to download a song
+url = input("Enter a URL (YouTube only): ")
+
+try:
+    video = YouTube(url.strip())
+except RegexMatchError:
+    stderr.write("Invalid URL\n")
+    exit(1)
+except VideoUnavailable:
+    stderr.write("Video not available\n")
+    exit(1)
+
+stream = video.streams.get_audio_only()
+print(stream.title)
+title = stream.title.replace(" ", "_") + ".m4a"
+stream.download("/home/sh/Downloads", title)
+
+# TODO - Add metadata to downloaded song
